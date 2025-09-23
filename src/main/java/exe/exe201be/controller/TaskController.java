391768c8d0ef093cfe66b72676440e96f1455d4e
Nghia Pham <minidora2707@gmail.com>
2025-09-23 15:38:39 +0700

    Fix Api

--- src/main/java/exe/exe201be/controller/TaskController.java
+++ src/main/java/exe/exe201be/controller/TaskController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/{projectId}/tasks")
+@RequestMapping("/api/{projectId}/tasks")
 public class TaskController {
     @Autowired
     private TaskService taskService;
